64232254ffbf5d457e4ad10e7f67bf852a990849
Joshua <joshua+github@frontlawn.net>
2023-06-27 23:54:16 -0700

    chore(deps): Update dependencies.

--- src/etc/constants.ts
+++ src/etc/constants.ts
@@ -1,26 +1,16 @@
 /**
  * Comma-delimited list of TypeScript extensions we want ESLint to include.
  */
-export const TS_EXTS = [
-  'ts',
-  'tsx',
-  'cts',
-  'mts'
-].join(',');
+export const TS_EXTS = 'ts,tsx,cts,mts' as const;
 
 
 /**
  * Comma-delimited list of JavaScript extensions we want ESLint to include.
  */
-export const JS_EXTS = [
-  'js',
-  'jsx',
-  'cjs',
-  'mjs'
-].join(',');
+export const JS_EXTS = 'js,jsx,cjs,mjs' as const;
 
 
 /**
  * Comma-delimited list of all extensions we want ESLint to include.
  */
-export const ALL_EXTS = [TS_EXTS, JS_EXTS].join(',');
+export const ALL_EXTS = `${TS_EXTS},${JS_EXTS}` as const;
