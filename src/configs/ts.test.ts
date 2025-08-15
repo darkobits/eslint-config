83abec0dd5ee0b8d4daf3a9c23731fddbfeae912
Joshua <joshua@frontlawn.net>
2025-08-14 18:57:09 -0700

    chore: Update CI workflow.

--- src/configs/ts.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-
-import { ts } from './ts'
-
-describe('preset-ts', () => {
-  it('should match the snapshot', () => {
-    expect(ts).toMatchSnapshot()
-  })
-})
--- src/configs/tsx.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-
-import { tsx } from './tsx'
-
-describe('preset-tsx', () => {
-  it('should match the snapshot', () => {
-    expect(tsx).toMatchSnapshot()
-  })
-})
