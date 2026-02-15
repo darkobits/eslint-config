5a8fae02bde32c29a0c04b50cc37beb4744f202e
Joshua <joshua@frontlawn.net>
2026-02-14 19:54:47 -0800

    chore: Update snapshots.

--- src/snapshot.test.ts
+++ src/snapshot.test.ts
@@ -6,7 +6,20 @@ import { configs } from './index'
 
 async function computeConfigForThisFile(config: ReturnType<typeof defineConfig>): Promise<Config> {
   const eslint = new ESLint({ overrideConfigFile: true, baseConfig: config })
-  return eslint.calculateConfigForFile(import.meta.filename)
+  const computedConfig = await eslint.calculateConfigForFile(import.meta.filename)
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    Reflect.deleteProperty(computedConfig, 'gitignorePath')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    Reflect.deleteProperty(computedConfig?.languageOptions?.parserOptions ?? {}, 'tsconfigRootDir')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    Reflect.deleteProperty(computedConfig?.settings['import/resolver']?.typescript, 'project')
+  } catch (error: any) {
+    // eslint-disable-next-line no-console
+    console.error(`Failed to remove tsconfigRootDir from config object:`, error)
+  }
+
+  return computedConfig
 }
 
 describe('configs', () => {
